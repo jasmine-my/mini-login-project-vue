e7587bb867677c14b396692d5199f7c0fbe1b061
nae_yeonkyeong <yeonkyeong@vgen.co.kr>
2022-07-21 00:08:08 +0900

    add: MyInput 컴포넌트 생성

---
+++ src/components/MyInput/MyInput.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="inputWrap">
+    <h3 v-if="title">{{title}}: </h3>
+    <input type="text"
+           id="inputId" @input="$emit('inputValueChange', $event)"
+    />
+  </div>
+  <p v-if="!isError">{{errorMessage}}</p>
+</template>
+
+<script>
+export default {
+  name: 'MyInput',
+  props: {
+    title: String,
+    inputId: String,
+    errorMessage: String,
+    isError: Boolean
+  },
+  data(){
+    return {
+      inputValue: ''
+    }
+  },
+}
+</script>
+
+<style scoped>
+  input {
+    border: 2px solid #2c3e50;
+  }
+  input:is(:active, :hover, :focus) {
+    border-color: #42b983;
+  }
+  .inputWrap {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
