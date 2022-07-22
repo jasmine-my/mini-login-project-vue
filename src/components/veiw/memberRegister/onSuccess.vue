7c180a1531f68a2ea0bf92c8bf34812f0685b285
nae_yeonkyeong <yeonkyeong@vgen.co.kr>
2022-07-23 00:30:50 +0900

    add: 완성화면 생성

---
+++ src/components/veiw/memberRegister/onSuccess.vue
@@ -0,0 +1,36 @@
+<template>
+  <p>🎉</p>
+  <h2 class="header">{{name}}님<br> 회원가입 되었습니다.</h2>
+
+  <ul>
+    <li><span>이메일</span>: {{email}}</li>
+    <li><span>주소</span>: {{address}}</li>
+    <li><span>연락처</span>: {{contract}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'OnSuccess',
+  props: {
+    name: String,
+    email: String,
+    address: String,
+    contract: String
+  },
+}
+</script>
+
+<style scoped>
+p{
+  text-align: center;
+  font-size: 50px;
+}
+ul > li {
+  list-style: none;
+  margin-bottom: 10px;
+}
+span {
+  font-weight: bold;
+}
+</style>
